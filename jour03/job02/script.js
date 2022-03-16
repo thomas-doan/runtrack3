905e127506662710492fd8a16b760bbf7da41f21
doan <thomas.doan@laplateforme.io>
2022-03-16 19:39:39 +0100

    afficher la validation

--- jour03/job02/script.js
+++ jour03/job02/script.js
@@ -7,10 +7,9 @@ $(document).ready(function() {
     let img5 = $("#img5");
     let img6 = $("#img6");
 
-    let resultatvalide = [1, 2, 3, 4, 5];
+    let resultatvalide = [1, 2, 3, 4, 5, 6];
     let result = [];
 
-    console.log($("#rangees")[0]);
     btn.click(function() {
         $(function() {
             var parent = $("#melangees");
@@ -25,7 +24,22 @@ $(document).ready(function() {
 
     $("img").on("click", function() {
         let valeur = $(this).attr("id");
+        result.push($(this).attr("id"));
         let range = $("#rangees");
         $("#rangees").append($("#" + valeur));
+
+        return result;
     });
+
+    if (result.length == 6) {
+        for (let i = 0; i < result.length; i++) {
+            for (let j = 0; j < resultatvalide.length; j++) {
+                if (result[i] == resultatvalide[j]) {
+                    console.log("victoire");
+                } else {
+                    console.log("non");
+                }
+            }
+        }
+    }
 });
