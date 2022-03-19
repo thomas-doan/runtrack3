67ef10c5a5b0dbbe91936fc50a842cdb86b9ac76
doan <thomas.doan@laplateforme.io>
2022-03-19 12:49:19 +0100

    maj de l'exo 3 jour 4

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="script.js"></script>
-    <title>Document</title>
+    <title>pokemon</title>
 </head>
 
 <body>
@@ -19,7 +19,7 @@
         </datalist>
     </form>
     <input id="trier" type="button" value="trier">
-
+    <p id="error"></p>
     <table>
 
     </table>
--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,10 +1,32 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     datalist = document.querySelector("datalist");
-    let type = document.querySelector("#type");
+    let typess = document.querySelector("#type");
     let btn = document.querySelector("#trier");
-
     let table = [];
     let table_list = [];
+
+    function create_pokemon(obj) {
+        let tbl = document.querySelector("table");
+        let tr = document.createElement("tr");
+
+        tr.id = obj.name["french"];
+        tbl.append(tr);
+        let tdName = document.createElement("td");
+        tdName.innerHTML = obj.name["french"];
+        let tdId = document.createElement("td");
+        tdId.innerHTML = obj.id;
+        let tdType = document.createElement("td");
+
+        if (obj.type[1] == undefined) {
+            tdType.innerHTML = obj.type[0];
+        } else {
+            tdType.innerHTML = obj.type[0] + " " + " " + obj.type[1];
+        }
+
+        tr.append(tdName);
+        tr.append(tdId);
+        tr.append(tdType);
+    }
 
     let liste = fetch("pokemon.json")
         .then((response) => response.json())
@@ -29,29 +51,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
             }
             btn.addEventListener("click", function() {
                 var type_select = type.value;
+                var id_pokemon = id.value;
 
                 response.forEach((pokemon) => {
                     if (pokemon.type.includes(type_select)) {
-                        let tbl = document.querySelector("table");
-                        let tr = document.createElement("tr");
-
-                        tr.id = pokemon.name["french"];
-                        tbl.append(tr);
-                        let tdName = document.createElement("td");
-                        tdName.innerHTML = pokemon.name["french"];
-                        let tdId = document.createElement("td");
-                        tdId.innerHTML = pokemon.id;
-                        let tdType = document.createElement("td");
-
-                        if (pokemon.type[1] == undefined) {
-                            tdType.innerHTML = pokemon.type[0];
-                        } else {
-                            tdType.innerHTML = pokemon.type[0] + " " + " " + pokemon.type[1];
-                        }
-
-                        tr.append(tdName);
-                        tr.append(tdId);
-                        tr.append(tdType);
+                        create_pokemon(pokemon);
+                    } else if (pokemon.id == id_pokemon) {
+                        create_pokemon(pokemon);
+                    } else {
+                        let error = document.querySelector("#error");
+                        error.innerHTML = "le pokemon n'existe pas !";
                     }
                 });
             });
